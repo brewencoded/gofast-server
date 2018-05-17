6f0dad84580334d8f0d91b2bfd3e12ad30d7880b
brewencoded <brewencoded@gmail.com>
2018-05-16 20:23:07 -0500

    feat(route): search route

--- main.py
+++ main.py
@@ -15,4 +15,10 @@ async def index(request):
     return html(index_page.read())
 
 
+@app.route('/api/search')
+async def get_all(request):
+    mock_inventory = open(os.getcwd() + '/test/mocks/inventory.json')
+    return json(mock_inventory)
+
+
 app.run(host="0.0.0.0", port=5000, debug=True)
